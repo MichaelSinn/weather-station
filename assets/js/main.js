c90c5a36f446d97836344f6e86562516fedd162f
MichaelSinn <michaelsinn.contact@gmail.com>
2022-09-26 15:44:09 -0400

    Added basic styling and structure to forecast

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,14 +33,17 @@ function getWeather(lat, lon, type) {
           let dailyWeather = [];
           let currentDay = [];
           // Split the data into separate days
-          data.list.forEach(e => {
-            if (currentDay.length === 0){
+          data.list.forEach((e) => {
+            if (currentDay.length === 0) {
               currentDay.push(e);
-            }else{
-              if (currentDay[currentDay.length - 1].dt_txt.split(" ")[0] !== e.dt_txt.split(" ")[0]){
+            } else {
+              if (
+                currentDay[currentDay.length - 1].dt_txt.split(" ")[0] !==
+                e.dt_txt.split(" ")[0]
+              ) {
                 dailyWeather.push(currentDay);
                 currentDay = [e];
-              }else{
+              } else {
                 currentDay.push(e);
               }
             }
@@ -50,7 +53,7 @@ function getWeather(lat, lon, type) {
 
           console.log(dailyWeather);
 
-          dailyWeather.forEach(e => {
+          dailyWeather.forEach((e) => {
             // Process each day's weather and add it to the respective card
 
             // TODO: Iterate over each of the hour segments for each day in order to find the averages and high/low temps etc
@@ -61,23 +64,40 @@ function getWeather(lat, lon, type) {
             weather = e[0].weather[0].description; // Mode
             windSpeed = e[0].wind.speed; // Mean
             humidity = e[0].main.humidity; // Mode
-            temperatureRange = [ // Max and min
+            temperatureRange = [
+              // Max and min
               Math.round(e[0].main.temp_min),
               Math.round(e[0].main.temp_max),
             ];
 
             let weatherCard = $("<article>");
+            let dateEl = $("<h3>");
+            let weatherListEl = $("<ul>");
+            let weatherEl = $("<li>");
+            let iconEl = $("<i>");
+            let windSpeedEl = $("<li>");
+            let humidityEl = $("<li>");
+            let temperatureRangeEl = $("<li>");
+            let fiveDayForecastEl = $("#five-day-forcast");
+
+            dateEl.text(weatherDate);
+            weatherEl.text(weather);
+            windSpeedEl.text(windSpeed + "km/h");
+            humidityEl.text(humidity + "%");
+            temperatureRangeEl.text(temperatureRange);
+            iconEl.text(weatherIcon);
+
+            weatherListEl.append(weatherEl);
+            weatherListEl.append(windSpeedEl);
+            weatherListEl.append(humidityEl);
+            weatherListEl.append(temperatureRangeEl);
+            
             weatherCard.addClass("col-12 col-md-4 col-lg-2 forcast-card");
-            weatherCard.text(
-              `${cityName} 
-              ${weatherDate} 
-              ${weather} 
-              ${weatherIcon} 
-              ${windSpeed}km/h 
-              ${humidity}% 
-              ${temperatureRange}`
-            );
-            $("#five-day-forcast").append(weatherCard);
+            weatherCard.append(dateEl);
+            weatherCard.append(iconEl);
+            weatherCard.append(weatherListEl);
+            
+            fiveDayForecastEl.append(weatherCard);
           });
 
           break;
@@ -97,26 +117,43 @@ function getWeather(lat, lon, type) {
 
           break;
       }
-
-      // 5 Day forecast (including today)
     });
 }
 
-function getAverage(dataList, type){
-  switch(type){
+function getCoords(cityName, provinceCode, countryCode) {
+  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${provinceCode},${countryCode}&limit=5&appid=${apiKey}`;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      if (data.length === 0){
+        console.log("No results.")
+        return;
+      }
+      let lat = data[0].lat;
+      let lon = data[0].lon;
+      getWeather(lat, lon, "weather");
+      getWeather(lat, lon, "forecast");
+    });
+}
+
+function getAverage(dataList, type) {
+  switch (type) {
     case "mode":
       let count = {};
-      dataList.forEach(e => {
-        if (!(e in count)){
+      dataList.forEach((e) => {
+        if (!(e in count)) {
           count[e] = 0;
         }
-        count ++;
+        count++;
       });
 
       let mostCommon = 0;
       let mostCommonElement;
       Object.entries(count).forEach(([key, value]) => {
-        if (v > mostCommon){
+        if (v > mostCommon) {
           mostCommonElement = key;
           mostCommon = value;
         }
@@ -125,7 +162,7 @@ function getAverage(dataList, type){
       return mostCommonElement;
     case "mean":
       let total = 0;
-      dataList.forEach(e => {
+      dataList.forEach((e) => {
         total += e;
       });
       return total / dataList.length;
@@ -133,4 +170,5 @@ function getAverage(dataList, type){
   return null;
 }
 
+// getCoords("oakville", "on", "ca");
 getWeather("43.46", "-79.66", "forecast"); // Current Weather
