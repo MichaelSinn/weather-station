63f6cc1db4be1cef1c8d62fd42180b6e63e08af8
MichaelSinn <michaelsinn.contact@gmail.com>
2022-09-26 21:43:20 -0400

    Added more styling, and high/low temps for forecast

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 /* 
   TODO:
     - Search history
-    - High/low temps for forecast
     - Style page
+    - Handling multiple cities being returned
 */
 
 const apiKey = "4525e4c4d6900be2e3932d311208c64e";
+const searchButton = $("#search-button");
 
 let searchHistory;
 
@@ -23,10 +24,13 @@ function getWeather(lat, lon, type) {
       let weather;
       let weatherDate;
       let weatherIcon;
-      console.log(data);
+      // Reset all the weather cards
+
       // Process all of the data here and append it to the page
       switch (type) {
         case "forecast": // 120 hours of weather split into 3 hour segments
+          const fiveDayForecastEl = $("#five-day-forcast");
+          fiveDayForecastEl.html("");
           let dailyWeather = [];
           let currentDay = [];
           // Split the data into separate days
@@ -34,10 +38,7 @@ function getWeather(lat, lon, type) {
             if (currentDay.length === 0) {
               currentDay.push(e);
             } else {
-              if (
-                currentDay[currentDay.length - 1].dt_txt.split(" ")[0] !==
-                e.dt_txt.split(" ")[0]
-              ) {
+              if (currentDay[currentDay.length - 1].dt_txt.split(" ")[0] !== e.dt_txt.split(" ")[0]) {
                 dailyWeather.push(currentDay);
                 currentDay = [e];
               } else {
@@ -54,47 +55,60 @@ function getWeather(lat, lon, type) {
             // Process each day's weather and add it to the respective card
 
             // TODO: Iterate over each of the hour segments for each day in order to find the averages and high/low temps etc
-            weatherDate = new Date(e[4].dt * 1000).toJSON().split("T")[0];
-            weatherIcon = e[4].weather[0].icon;
+            weatherDate = new Date(e[0].dt * 1000).toJSON().split("T")[0];
+            if (e[5]){
+              weatherIcon = e[5].weather[0].icon;
 
-            weather = e[4].weather[0].description; // Mode
-            windSpeed = e[4].wind.speed; // Mean
-            humidity = e[4].main.humidity; // Mode
-            temperatureRange = `Low ${Math.round(e[4].main.temp_min)}°C / High ${Math.round(e[4].main.temp_max)}°C`
+              weather = e[5].weather[0].description; // Mode
+              windSpeed = e[5].wind.speed; // Mean
+              humidity = e[5].main.humidity; // Mode
+            }else{
+              weatherIcon = e[0].weather[0].icon;
+
+              weather = e[0].weather[0].description; // Mode
+              windSpeed = e[0].wind.speed; // Mean
+              humidity = e[0].main.humidity; // Mode
+            }
+            temperatureRangeData = getMaxMinTemperature(e);
+            temperatureRange = `Low ${temperatureRangeData.low}°C / High ${temperatureRangeData.high}°C`; // max/min
 
             let weatherCard = $("<article>");
-            let dateEl = $("<h3>");
+            let dateEl = $("<h4>");
             let weatherListEl = $("<ul>");
             let weatherEl = $("<li>");
             let iconEl = $("<img>");
             let windSpeedEl = $("<li>");
             let humidityEl = $("<li>");
             let temperatureRangeEl = $("<li>");
-            let fiveDayForecastEl = $("#five-day-forcast");
 
             dateEl.text(weatherDate);
             weatherEl.text(weather);
-            windSpeedEl.text(windSpeed + "km/h");
+            windSpeedEl.text(windSpeed + "m/s");
             humidityEl.text(humidity + "%");
             temperatureRangeEl.text(temperatureRange);
-            iconEl.attr("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
+            iconEl.attr(
+              "src",
+              `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+            );
 
             weatherListEl.append(weatherEl);
             weatherListEl.append(windSpeedEl);
             weatherListEl.append(humidityEl);
             weatherListEl.append(temperatureRangeEl);
-            
+
             weatherCard.addClass("col-12 col-md-4 col-lg-2 forcast-card");
             weatherCard.append(dateEl);
             weatherCard.append(iconEl);
             weatherCard.append(weatherListEl);
-            
+
             fiveDayForecastEl.append(weatherCard);
           });
 
           break;
 
         case "weather": // Current weather
+          const todaysForecastEl = $("#todays-forecast");
+          todaysForecastEl.html("");
           let currentTemp = Math.round(data.main.temp);
           let cityName = data.name;
           weatherDate = new Date(data.dt * 1000).toJSON().split("T")[0];
@@ -102,61 +116,67 @@ function getWeather(lat, lon, type) {
           weatherIcon = data.weather[0].icon;
           windSpeed = data.wind.speed;
           humidity = data.main.humidity;
-          temperatureRange = `Low ${Math.round(data.main.temp_min)}°C / High ${Math.round(data.main.temp_max)}°C`
-          
+          temperatureRange = `Low ${Math.round(
+            data.main.temp_min
+          )}°C / High ${Math.round(data.main.temp_max)}°C`;
+
           let weatherCard = $("<article>");
-          let cityNameEl = $("<h2>");
-          let dateEl = $("<h3>");
+          let cityNameEl = $("<h3>");
+          let dateEl = $("<h4>");
           let weatherListEl = $("<ul>");
           let weatherEl = $("<li>");
           let iconEl = $("<img>");
           let windSpeedEl = $("<li>");
           let humidityEl = $("<li>");
           let temperatureRangeEl = $("<li>");
-          let todaysForecastEl = $("#todays-forecast");
           let currentTempEl = $("<li>");
 
           cityNameEl.text(cityName);
           currentTempEl.text(currentTemp + "°C");
           dateEl.text(weatherDate);
           weatherEl.text(weather);
-          windSpeedEl.text(windSpeed + "km/h");
+          windSpeedEl.text(windSpeed + "m/s");
           humidityEl.text(humidity + "%");
           temperatureRangeEl.text(temperatureRange);
-          iconEl.attr("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
+          iconEl.attr(
+            "src",
+            `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+          );
 
           weatherListEl.append(weatherEl);
           weatherListEl.append(currentTempEl);
           weatherListEl.append(windSpeedEl);
           weatherListEl.append(humidityEl);
           weatherListEl.append(temperatureRangeEl);
-          
+
           weatherCard.addClass("col-12 col-md-10 col-lg-10 weather-card");
           weatherCard.append(cityNameEl);
           weatherCard.append(dateEl);
           weatherCard.append(iconEl);
           weatherCard.append(weatherListEl);
-          
+
           todaysForecastEl.append(weatherCard);
           break;
       }
     });
 }
 
-function getCoords(cityName, provinceCode, countryCode) {
-  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${provinceCode},${countryCode}&limit=5&appid=${apiKey}`;
+function getCoords(cityName, provinceCode = "", countryCode = "") {
+  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName.trim()},${provinceCode.trim()},${countryCode.trim()}&limit=5&appid=${apiKey}`;
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      if (data.length === 0){
-        console.log("No results.")
+      if (data.length === 0) {
+        console.log("No results.");
         return;
+      } else if (data.length > 1) {
+        // Open and modal of some kind to choose between the cities
       }
       let lat = data[0].lat;
       let lon = data[0].lon;
+
       getWeather(lat, lon, "weather");
       getWeather(lat, lon, "forecast");
     });
@@ -193,5 +213,32 @@ function getAverage(dataList, type) {
   return null;
 }
 
-getCoords("oakville", "on", "ca");
-// getWeather("43.46", "-79.66", "forecast"); // Current Weather
+function getMaxMinTemperature(weatherData){
+  let lowest;
+  let highest;
+  weatherData.forEach(e => {
+    if (!lowest || !highest){
+      lowest = e.main.temp_min;
+      highest = e.main.temp_max;
+    }
+    if (e.main.temp_max > highest) highest = e.main.temp_max;
+    if (e.main.temp_min < lowest) lowest = e.main.temp_min;
+  })
+  return {
+    high: Math.round(highest),
+    low: Math.round(lowest)
+  };
+}
+
+function updateSearchHistory(){
+
+}
+
+searchButton.on("click", function (e) {
+  e.preventDefault();
+  let content = $(this).parent().children().eq(0).val();
+  let args = content.split(",");
+  getCoords(args[0], (provinceCode = args[1]), (countryCode = args[2]));
+});
+
+getCoords("toronto", "on", "ca")// Display Toronto, Ontario's weather as default weather
